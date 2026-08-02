c429f021f27a8aff4ae23b2474c0ddb8f9b30451
Moksh-10 <agmoksh10@gmail.com>
2026-08-02 08:24:34 +0530

    .

--- server/sync_tcp.go
+++ server/sync_tcp.go
@@ -5,24 +5,40 @@ import (
 	"strconv"
 	"log"
 	"io"
+	"fmt"
+	"strings"
 
 	"github.com/Moksh-10/redis/config"
+	"github.com/Moksh-10/redis/core"
 )
 
-func readCommand(c net.Conn) (string, error) {
+func readCommand(c net.Conn) (*core.RedisCmd, error) {
 	var buf []byte = make([]byte, 512)
 	n, err := c.Read(buf[:])
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return string(buf[:n]), nil 
+
+	tokens, err := core.DecodeArrayString(buf[:n])
+	if err != nil {
+		return nil, err
+	}
+
+	return &core.RedisCmd{
+		Cmd: strings.ToUpper(tokens[0]),
+		Args: tokens[1:],
+	}, nil
 }
 
-func respond(cmd string, c net.Conn) error {
-	if _, err := c.Write([]byte(cmd)); err != nil {
-		return err
+func respondError(err error, c net.Conn) {
+	c.Write([]byte(fmt.Sprintf("-%s\r\n", err)))
+}
+
+func respond(cmd *core.RedisCmd, c net.Conn) {
+	err := core.EvalAndRespond(cmd, c)
+	if err != nil {
+		respondError(err, c)
 	}
-	return nil 
 }
 
 func RunSyncTCPServer() {
@@ -55,10 +71,7 @@ func RunSyncTCPServer() {
 				}
 				log.Println("err", err)
 			}
-			log.Println("command: ", cmd)
-			if err = respond(cmd, c); err != nil {
-				log.Print("err write: ", err)
-			}
+			respond(cmd, c)
 		}
 	}
 }
