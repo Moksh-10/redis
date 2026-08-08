8261866beec8cc44183e46d6798ed22e67c9d6b0
Moksh-10 <agmoksh10@gmail.com>
2026-08-08 18:39:17 +0530

    .

--- core/eval.go
+++ core/eval.go
@@ -42,6 +42,7 @@ func evalSET(args []string) []byte {
 	var exDurationMs int64 = -1
 
 	key, value = args[0], args[1]
+	oType, oEnc := deduceTypeEncoding(value)
 
 	for i := 2; i<len(args); i++ {
 		switch args[i] {
@@ -61,7 +62,7 @@ func evalSET(args []string) []byte {
 		}
 	}
 
-	Put(key, NewObj(value, exDurationMs))
+	Put(key, NewObj(value, exDurationMs, oType, oEnc))
 	return RESP_OK
 }
 
@@ -150,6 +151,33 @@ func evalBGREWRITEAOF(args []string) []byte {
 	return RESP_OK
 }
 
+func evalINCR(args []string) []byte {
+	if len(args) != 1 {
+		return Encode(errors.New("wrong no. of args for 'incr'"), false)
+	}
+
+	var key string = args[0]
+	obj := Get(key)
+	if obj == nil {
+		obj = NewObj("0", -1, OBJ_TYPE_STRING, OBJ_ENCODING_INT)
+		Put(key, obj)
+	}
+
+	if err := assertType(obj.TypeEncoding, OBJ_TYPE_STRING); err != nil {
+		return Encode(err, false)
+	}
+
+	if err := assertEncoding(obj.TypeEncoding, OBJ_ENCODING_INT); err != nil {
+		return Encode(err, false)
+	}
+
+	i, _ := strconv.ParseInt(obj.Value.(string), 10, 64)
+	i++
+	obj.Value = strconv.FormatInt(i, 10)
+
+	return Encode(i, false)
+}
+
 func EvalAndRespond(cmds RedisCmds, c io.ReadWriter) {
 	var response []byte 
 	buf := bytes.NewBuffer(response)
@@ -169,6 +197,8 @@ func EvalAndRespond(cmds RedisCmds, c io.ReadWriter) {
 			buf.Write(evalEXPIRE(cmd.Args))
 		case "BGREWRITEAOF":
 			buf.Write(evalBGREWRITEAOF(cmd.Args))
+		case "INCR":
+			buf.Write(evalINCR(cmd.Args))
 		default:
 			buf.Write(evalPING(cmd.Args))
 		}
--- core/typeencoding.go
+++ core/typeencoding.go
@@ -9,3 +9,17 @@ func getType(te uint8) uint8 {
 func getEncoding(te uint8) uint8 {
 	return te & 0b00001111
 }
+
+func assertType(te uint8, t uint8) error {
+	if getType(te) != t {
+		return errors.New(("the operations is not permitted on this type"))
+	}
+	return nil
+}
+
+func assertEncoding(te uint8, e uint8) error {
+	if getEncoding(te) != e {
+		return errors.New("the operation is not permitted on this encoding")
+	}
+	return nil
+}
